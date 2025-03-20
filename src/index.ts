6fecf653ab308d59f6f0fa1a29b307062a2be3cd
Manie-oss <to.mansip@gmail.com>
2025-03-20 22:56:02 +0530

    added initial route

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,25 @@ import route from "./routes";
 import dotenv from "dotenv";
 dotenv.config();
 
-const MONGODB_URL = process.env.MONGODB_URL || '';
+const MONGODB_URL = process.env.MONGODB_URL || "";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use('/api', route);
 
-mongoose.connect(MONGODB_URL)
-.then((con)=> console.log('db is connected.', con.connection.host))
-.then(() => app.listen(PORT, () => console.log(`Server is running on the port ${PORT}`)))
-.catch((err) => console.log(err.message));
+app.get("/", (req, res) => {
+  res.send("Welcome to Code share");
+});
+
+app.use("/api", route);
+
+mongoose
+  .connect(MONGODB_URL)
+  .then((con) => console.log("db is connected.", con.connection.host))
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on the port ${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
 
 export default app;
